fa8fa790e3599e8228f729cfb4a405c98d5023a5
CyJay <cyjaysong@foxmail.com>
2025-06-20 17:22:21 +0800

    🐛 图片上传支持从网络资源下载然后上传

--- func_file.go
+++ func_file.go
@@ -1,9 +1,24 @@
 package globebill
 
-import "github.com/cyjaysong/globebill-go/model"
+import (
+	"fmt"
+	"github.com/cyjaysong/globebill-go/model"
+	"github.com/deatil/go-cryptobin/tool/encoding"
+)
 
 // FileUpload 上传媒体图片
 func (t Client) FileUpload(reqBody model.FileUploadReq) (resBody *model.MerchantBaseRes[model.FileUploadRes], err error) {
+	if reqBody.FileData == "" && reqBody.FileUrl != "" {
+		if response, err := t.reqClient.R().Get(reqBody.FileUrl); err != nil {
+			return nil, err
+		} else if response.IsErrorState() {
+			return nil, fmt.Errorf("load file use url err %s", response.Status)
+		} else if len(response.Bytes()) == 0 {
+			return nil, fmt.Errorf("load file use url fail")
+		} else {
+			reqBody.FileData = encoding.Base64Encode(response.Bytes())
+		}
+	}
 	resBody = new(model.MerchantBaseRes[model.FileUploadRes])
 	if err = t.merchantJsonPost("/Api/Access/FileInfo/Upload", reqBody, resBody); err != nil {
 		return nil, err
--- model/file.go
+++ model/file.go
@@ -3,6 +3,7 @@ package model
 // FileUploadReq 上传媒体图片
 type FileUploadReq struct {
 	FileData string `json:"fileData"` // 图片Base64
+	FileUrl  string `json:"-"`        // 用于使用网络资源图片上传
 }
 
 // FileUploadRes 上传媒体图片
